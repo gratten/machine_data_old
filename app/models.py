ebc4301b9f7009d21f50fcd68d04745e06c4957e
gratten <grattenward@gmail.com>
2020-09-26 17:13:26 -0400

    db setup, add machine page functional

--- app/models.py
+++ app/models.py
@@ -4,8 +4,19 @@ class Machine(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     project = db.Column(db.String(8), index=True, unique=True)
     serialnum = db.Column(db.String(64), index=True, unique=True)
-    customer = db.Column(db.String(64), index=True, unique=True)
-    description = db.Column(db.String(128), index=True, unique=True)
+    customer = db.Column(db.String(64))
+    description = db.Column(db.String(128))
+    kits = db.relationship('Kit', backref='parent', lazy='dynamic')
 
     def __repr__(self):
-        return f'<Machine {self.serialnum}'
+        return f'<Machine {self.serialnum}>'
+
+class Kit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    length = db.Column(db.String(8))
+    width = db.Column(db.String(8))
+    depth = db.Column(db.String(8))
+    machine_id = db.Column(db.Integer, db.ForeignKey('machine.id'))
+
+    def __repr__(self):
+        return f'<Kit {self.length}x{self.width}x{self.depth}>'
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,25 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import AddMachineForm
+from app.models import Machine
+from app import db
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    machines = Machine.query.all()
+    return render_template('index.html', title='Home', machines=machines)
 
 @app.route('/add_machine', methods=['GET', 'POST'])
 def add_machine():
     form = AddMachineForm()
     if form.validate_on_submit():
+        machine = Machine(project=form.project.data,
+                          serialnum=form.serialnum.data,
+                          customer=form.customer.data,
+                          description=form.description.data)
+        db.session.add(machine)
+        db.session.commit()
         flash('Machine added')
         return redirect(url_for('index'))
     return render_template('add_machine.html', title='Add Machine', form=form)
